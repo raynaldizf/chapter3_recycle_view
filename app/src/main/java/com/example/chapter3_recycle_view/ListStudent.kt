4485556ab9ac170018efb531647ab77e7f7eb42b
surezluvy <raynaldizoel@gmail.com>
2022-09-09 19:11:44 +0700

    Fix Error

--- app/src/main/java/com/example/chapter3_recycle_view/ListStudent.kt
+++ app/src/main/java/com/example/chapter3_recycle_view/ListStudent.kt
@@ -1,3 +1,3 @@
 package com.example.chapter3_recycle_view
 
-data class ListStudent(val image: Int, val nama: String, val nim: String)
+data class ListStudent(val nama: String, val nim: String, val image: Int)
